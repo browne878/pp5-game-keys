04fa86b5b00397751f5f624c933017b1c08d3e5f
Aaron Browne <dragon015789@gmail.com>
2023-02-24 21:12:33 +0000

    Add subscribed confirmation email

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+
 from .forms import SubscriptionForm
 from .models import Subscription
 
@@ -12,6 +17,19 @@ def newsletter(request):
         form = SubscriptionForm(request.POST)
         if form.is_valid():
             form.save()
+            
+            name = request.POST['name']
+            email = request.POST['email']
+            html_message = render_to_string('email/subscribed.html')
+                            
+            send_mail(
+                f'Thank you for Subscribing {name}!',
+                strip_tags(html_message),
+                settings.DEFAULT_FROM_EMAIL,
+                [email],
+                html_message=html_message
+            )
+            
             return redirect(reverse('subscribed'))
         else:
             context = {
