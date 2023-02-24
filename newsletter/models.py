0847c87374f1652c079da905d3d0664400a4a3b6
Aaron Browne <dragon015789@gmail.com>
2023-02-24 17:42:11 +0000

    Create newsletter subscription model

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Subscription(models.Model):
+    """ Stores newsletter subscriptions """
+
+    name = models.CharField(max_length=254, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
