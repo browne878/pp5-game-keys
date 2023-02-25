9e0565bfead63302afd3ed9c1cd59f9624028f61
Aaron Browne <dragon015789@gmail.com>
2023-02-25 16:37:02 +0000

    Add tests to games view

--- games/tests.py
+++ games/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestGamesViews(TestCase):
+    """ Tests the views in the games app """
+    
+    def test_get_games_page(self):
+        """ Tests the games page loads correctly """
+        
+        page = self.client.get("/games/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "games/games.html")
+
