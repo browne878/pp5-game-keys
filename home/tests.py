5653490faf1dcd11339a3785cf821367bd86b640
Aaron Browne <dragon015789@gmail.com>
2023-02-25 16:24:08 +0000

    Add home page view test

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .views import index
 
-# Create your tests here.
+
+class TestHomePageViews(TestCase):
+
+    def test_get_home_page(self):
+        """
+        Test that the home page is returned
+        when accessing the index view
+        """
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "home/index.html")
