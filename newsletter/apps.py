e1ece6aa7d405de17bbaeb427a8c9b37bd597746
Aaron Browne <dragon015789@gmail.com>
2023-02-26 16:48:25 +0000

    remove send email on interval

--- newsletter/apps.py
+++ newsletter/apps.py
@@ -1,11 +1,11 @@
 from django.apps import AppConfig
-from asgiref.sync import sync_to_async
+# from asgiref.sync import sync_to_async
 
 
 class NewsletterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'newsletter'
     
-    def ready(self):
-        from . import tasks
-        sync_to_async(tasks.interval_schedule)()
+    # def ready(self):
+    #     from . import tasks
+    #     sync_to_async(tasks.interval_schedule)()
